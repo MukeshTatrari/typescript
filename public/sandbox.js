d0b0d95c866194366348254b4a00f93485b87e00
Chandra <Mukesh.Chandra@bsci.com>
2021-06-04 18:38:53 +0530

    changes

--- public/sandbox.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var character = "hello";
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
-console.log("test");
