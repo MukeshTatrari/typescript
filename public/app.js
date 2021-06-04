147e0790df74a31acc3d30e6a10f7a40d6504e6f
Chandra <Mukesh.Chandra@bsci.com>
2021-06-04 19:00:56 +0530

    changes

--- public/app.js
+++ public/app.js
@@ -1,11 +1,30 @@
 "use strict";
-var form = document.querySelector('.new-item-form');
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('Mukesh', 'work on the Mukesh website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(function (inv) {
+    console.log(inv.client, inv.details, inv.amount, inv.format());
+});
+var form = document.querySelector(".new-item-form");
 //inputs
 var type = document.querySelector("#type");
 var tofrom = document.querySelector("#tofrom");
 var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(type.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,43 @@
-const form = document.querySelector('.new-item-form')! as HTMLFormElement;
+class Invoice {
+  readonly client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice('mario','work on the mario website', 250);
+const invTwo = new Invoice('Mukesh','work on the Mukesh website', 300);
+
+
+
+let invoices: Invoice[]=[];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach(inv=>{
+    console.log(inv.client,inv.details,inv.amount,inv.format());
+})
+
+const form = document.querySelector(".new-item-form")! as HTMLFormElement;
 
 //inputs
 
-const type = document.querySelector("#type") ! as HTMLSelectElement;
+const type = document.querySelector("#type")! as HTMLSelectElement;
 
-const tofrom  = document.querySelector("#tofrom") ! as HTMLInputElement;
-const details  = document.querySelector("#details") ! as HTMLInputElement;
-const amount  = document.querySelector("#amount") ! as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom")! as HTMLInputElement;
+const details = document.querySelector("#details")! as HTMLInputElement;
+const amount = document.querySelector("#amount")! as HTMLInputElement;
 
-form.addEventListener('submit',(e:Event)=>{
-    e.preventDefault();
-    console.log(
-        type.value,
-        details.value,
-        amount.valueAsNumber
-    )
-})
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log(type.value, details.value, amount.valueAsNumber);
+});
